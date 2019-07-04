627c968f207a0cda8c170c3888ac71b2ee892090
kzish <zishumbak@gmail.com>
2019-07-04 11:04:10 +0100

    1

--- git_push.bat
+++ git_push.bat
@@ -3,4 +3,4 @@ git config --global user.name "kzish"
 git config --global user.password "QazWsxEdc@123" 
 git add .
 git commit -m "1"
-git push https://github.com/kzish/portchlytAPI.git
+git push --set-upstream https://github.com/kzish/portchlytAPI.git master
